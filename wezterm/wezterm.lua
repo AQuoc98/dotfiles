3ef3db7fcbf2bdb21b749dec5ef8625a6391d7ef
Nguyễn Đình Anh Quốc <ken@192.168.2.28.non-exists.ptr.local>
2024-12-21 19:49:16 +0700

    fix: fix theme for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,8 +7,9 @@ local themes = {
 	nord = "Nord (Gogh)",
 	onedark = "One Dark (Gogh)",
 }
-local success, stdout, stderr = wezterm.run_child_process({ os.getenv("SHELL"), "-c", "printenv WEZTERM_THEME" })
+local success, stdout, stderr = wezterm.run_child_process({ os.getenv("SHELL"), "-c", "echo $WEZTERM_THEME" })
 local selected_theme = stdout:gsub("%s+", "") -- Remove all whitespace characters including newline
-config.color_scheme = themes[selected_theme]
+
+config.color_scheme = themes[selected_theme] or themes['nord']
 
 return config
